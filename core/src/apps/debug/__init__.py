6a7fb40785008f97b29dd7607e63a30c44180241
grdddj <jiri.musil06@seznam.cz>
2022-05-18 11:36:29 +0200

    feat(tests): setup UI tests for model R
    [no changelog]

--- core/src/apps/debug/__init__.py
+++ core/src/apps/debug/__init__.py
@@ -6,7 +6,7 @@ if not __debug__:
 if __debug__:
     from storage import debug as storage
 
-    from trezor import log, loop, wire
+    from trezor import log, loop, utils, wire
     from trezor.ui import display
     from trezor.enums import MessageType
     from trezor.messages import (
@@ -148,7 +148,7 @@ if __debug__:
         x = msg.x  # local_cache_attribute
         y = msg.y  # local_cache_attribute
 
-        if x is not None and y is not None:
+        if x is not None and y is not None and utils.MODEL in ("T",):
             evt_down = io.TOUCH_START, x, y
             evt_up = io.TOUCH_END, x, y
             loop.synthetic_events.append((io.TOUCH, evt_down))
--- core/src/apps/homescreen/lockscreen.py
+++ core/src/apps/homescreen/lockscreen.py
@@ -1,5 +1,5 @@
 import storage.cache
-from trezor import loop, ui
+from trezor import loop, ui, utils
 
 from . import HomescreenBase
 
@@ -42,26 +42,39 @@ class Lockscreen(HomescreenBase):
         from trezor import res
         from trezor import ui  # local_cache_global
 
-        display = ui.display  # local_cache_attribute
-        title_grey = ui.TITLE_GREY  # local_cache_attribute
-        bg = ui.BG  # local_cache_attribute
+        if utils.MODEL in ("T",):
+            display = ui.display  # local_cache_attribute
+            title_grey = ui.TITLE_GREY  # local_cache_attribute
+            bg = ui.BG  # local_cache_attribute
 
-        # homescreen with label text on top
-        display.text_center(ui.WIDTH // 2, 35, self.label, ui.BOLD, title_grey, bg)
-        display.avatar(48, 48, self.get_image(), ui.WHITE, ui.BLACK)
+            # homescreen with label text on top
+            display.text_center(ui.WIDTH // 2, 35, self.label, ui.BOLD, title_grey, bg)
+            display.avatar(48, 48, self.get_image(), ui.WHITE, ui.BLACK)
 
-        # lock bar
-        display.bar_radius(40, 100, 160, 40, title_grey, bg, 4)
-        display.bar_radius(42, 102, 156, 36, bg, title_grey, 4)
-        display.text_center(
-            ui.WIDTH // 2, 128, self.lock_label, ui.BOLD, title_grey, bg
-        )
+            # lock bar
+            display.bar_radius(40, 100, 160, 40, title_grey, bg, 4)
+            display.bar_radius(42, 102, 156, 36, bg, title_grey, 4)
+            display.text_center(
+                ui.WIDTH // 2, 128, self.lock_label, ui.BOLD, title_grey, bg
+            )
 
-        # "tap to unlock"
-        display.text_center(
-            ui.WIDTH // 2 + 10, 220, self.tap_label, ui.BOLD, title_grey, bg
-        )
-        display.icon(45, 202, res.load(ui.ICON_CLICK), title_grey, bg)
+            # "tap to unlock"
+            display.text_center(
+                ui.WIDTH // 2 + 10, 220, self.tap_label, ui.BOLD, title_grey, bg
+            )
+            display.icon(45, 202, res.load(ui.ICON_CLICK), title_grey, bg)
+        elif utils.MODEL in ("R",):
+            ui.header_warning(self.lock_label)
+            icon = "trezor/res/homescreen_model_r.toif"  # 92x92 px
+            ui.display.icon(18, 18, ui.res.load(icon), ui.style.FG, ui.style.BG)
+            ui.display.text_center(
+                ui.WIDTH // 2, 120, self.label, ui.BOLD, ui.FG, ui.BG
+            )
+        elif utils.MODEL in ("1",):
+            ui.header_warning(self.lock_label)
+            icon = "trezor/res/homescreen_model_1.toif"  # 64x36 px
+            ui.display.icon(33, 14, ui.res.load(icon), ui.style.FG, ui.style.BG)
+            ui.display.text_center(ui.WIDTH // 2, 60, self.label, ui.BOLD, ui.FG, ui.BG)
 
     def on_touch_end(self, _x: int, _y: int) -> None:
         raise ui.Result(None)
